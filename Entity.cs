4262147f8f85f9674e914b1d898db0f36df2863b
TwinHter <nhdnguyen05@gmail.com>
2024-08-03 17:24:53 +0700

    .

---
+++ Entity.cs
@@ -0,0 +1,18 @@
+namespace HterGame {
+    public class Hero {
+        public int HP{ get;}
+
+    }
+    public class Monster {
+        public int HP{ get;}
+    }
+    public class Flying_Monster : Monster {
+
+    }
+    public class Ground_Monster : Monster {
+
+    }
+    public class Trader {
+
+    }
+}
